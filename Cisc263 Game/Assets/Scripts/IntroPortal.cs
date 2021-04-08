fd8a2a21d7f3e865216495890b842d67334c54f0
SLitwiniuk <68765886+SLitwiniuk@users.noreply.github.com>
2021-04-07 20:42:02 -0400

    Intro scene and transition scene

---
+++ Cisc263 Game/Assets/Scripts/IntroPortal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroPortal : MonoBehaviour
+{
+    //[SerializeField] private AudioSource introMusic;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.CompareTag("Player")){
+            Debug.Log("Portal Activated");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
