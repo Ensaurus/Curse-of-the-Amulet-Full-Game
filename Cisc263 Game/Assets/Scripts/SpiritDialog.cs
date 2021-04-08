502521c5eea7496129fcc94e4967344f4f14226e
SLitwiniuk <68765886+SLitwiniuk@users.noreply.github.com>
2021-04-07 22:48:52 -0400

    NPC interaction

---
+++ Cisc263 Game/Assets/Scripts/SpiritDialog.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpiritDialog : MonoBehaviour
+{
+    public GameObject NPCBubble;
+    public GameObject talkOption;
+    public GameObject noOption;
+
+
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.CompareTag("Player")){
+            //Debug.Log("NPC Activated");
+            Time.timeScale = 0f;
+            NPCBubble.SetActive(true);
+            talkOption.SetActive(true);
+            noOption.SetActive(true);
+        }
+    }
+
+    public void Talk(){
+        //Debug.Log("NPC Chatting");
+        NPCBubble.SetActive(false);
+        talkOption.SetActive(false);
+        noOption.SetActive(false);
+        SceneManager.LoadScene("NPC");
+    }
+
+    public void NoTalk(){
+        //Debug.Log("No NPC Chatting");
+        Time.timeScale = 1f;
+        NPCBubble.SetActive(false);
+        talkOption.SetActive(false);
+        noOption.SetActive(false);
+    }
+}
---
+++ Cisc263 Game/Assets/Scripts/SpiritInteraction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpiritInteraction : MonoBehaviour
+{
+
+    public GameObject next;
+    public GameObject R1;
+    public GameObject R2;
+    public GameObject R3;
+    public GameObject R4;
+    public GameObject R5;
+    public GameObject R6;
+    public GameObject R7;
+    public GameObject R8;
+    
+    public void nextQuestion(){
+        R1.SetActive(false);
+        R2.SetActive(false);
+        R3.SetActive(false);
+        R4.SetActive(false);
+        R5.SetActive(false);
+        R6.SetActive(false);
+        R7.SetActive(false);
+        R8.SetActive(false);
+    }
+    public void Question1(){
+        R1.SetActive(true);
+    }
+    public void Question2(){
+        R2.SetActive(true);
+    }
+    public void Question3(){
+        R3.SetActive(true);
+    }
+    public void Question4(){
+        R4.SetActive(true);
+    }public void Question5(){
+        R5.SetActive(true);
+    }
+    public void Question6(){
+        R6.SetActive(true);
+    }public void Question7(){
+        R7.SetActive(true);
+    }
+    public void Question8(){
+        R8.SetActive(true);
+    }
+
+//To Take you back to the intro scene
+    public void Done(){
+        SceneManager.LoadScene("IntroScene");
+    }
+}
